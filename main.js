747b366638136f835183dd550be1b5b3ffdcdf72
Imad Rahmouni <imad19rahmouni@gmail.com>
2023-06-04 11:01:19 +0200

    first

---
+++ main.js
@@ -0,0 +1,21 @@
+const menu = document.querySelector('.menu');
+const navOpen = document.querySelector('.hamburger');
+const navClose = document.querySelector('.close');
+
+
+const navLeft = menu.getBoundingClientRect().left;
+navOpen.addEventListener('click',()=>{
+    if(navLeft < 0){
+        menu.classList.add('show');
+        document.body.classList.add('show');
+        navBar.classList.add('show');
+    } 
+}).onclick(open)
+
+navClose.addEventListener('click', ()=>{
+    if(navLeft < 0){
+        menu.classList.remove('show');
+        document.body.classList.remove('show');
+        navBar.classList.remove('show');
+    } 
+}).onclick(close)
